506e3e7a1077f2deceabdb8c47a104aaa6d1545a
luo-hm <61671300+luo-hm@users.noreply.github.com>
2020-03-07 14:24:03 +0800

    Update HelloWord.java

--- test/src/hello/word/HelloWord.java
+++ test/src/hello/word/HelloWord.java
@@ -4,6 +4,8 @@ public class HelloWord {
 
 	public static void main(String[] args) {
 		System.out.println("HelloWord");
+		System.out.println("HelloWord");
+		System.out.println("HelloWord");
 	}
 }
 //这是新添加的注释
